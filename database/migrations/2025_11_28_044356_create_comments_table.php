2e734e4f98a7cd2baab19d828868949e737a78d1
MaiTruong1312 <maitruong1312205@gmail.com>
2025-12-07 23:34:47 +0700

    docker deploy

--- database/migrations/2025_11_28_044356_create_comments_table.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// This migration has been moved to 2025_12_05_094940_create_comments_table.php to fix ordering issues.
--- database/migrations/2025_11_28_054439_create_comment_likes_table.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// This migration has been moved to 2025_12_05_094941_create_comment_likes_table.php to fix ordering issues.
