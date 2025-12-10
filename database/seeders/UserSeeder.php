754f52eb53d6f2a4a3842292e14ce0246e86cb4a
MaiTruong1312 <maitruong1312205@gmail.com>
2025-12-10 16:33:48 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -28,7 +27,7 @@ class UserSeeder extends Seeder
                 'email' => 'employer@example.com',
                 'password' => Hash::make('password'),
                 'role' => 'employer',
-                'avatar' => 'avatars/692e8be107fbe.jpg',
+                'avatar' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
@@ -38,67 +37,10 @@ class UserSeeder extends Seeder
                 'email' => 'candidate@example.com',
                 'password' => Hash::make('password'),
                 'role' => 'candidate',
-                'avatar' => 'avatars/6924fbcdaf955.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 4,
-                'name' => 'Sơn Quý',
-                'email' => 'progamevip2310@gmail.com',
-                'password' => Hash::make('password'),
-                'role' => 'employer',
-                'avatar' => 'avatars/6929c9ab356fe.jpg',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 5,
-                'name' => 'Mai Văn Trường',
-                'email' => 'goodjobem2@gmail.com',
-                'password' => Hash::make('password'),
-                'role' => 'candidate',
                 'avatar' => null,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            [
-                'id' => 6,
-                'name' => 'Mai Văn Trường',
-                'email' => 'maitruong1312205@gmail.com',
-                'password' => Hash::make('password'),
-                'role' => 'employer',
-                'avatar' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 7,
-                'name' => 'Mai Văn Trường',
-                'email' => 'a@gmail.com',
-                'password' => Hash::make('password'),
-                'role' => 'employer',
-                'avatar' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'id' => 8,
-                'name' => 'Sơn Quý',
-                'email' => '26a4041674@hvnh.edu.vn',
-                'password' => Hash::make('password'),
-                'role' => 'candidate',
-                'avatar' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ]);
-
-        // Nếu bạn vẫn muốn tạo thêm user bằng factory
-        User::factory()->create([
-            'name' => 'Admin Factory',
-            'email' => 'factory_admin@example.com',
-            'role' => 'admin',
         ]);
     }
 }
